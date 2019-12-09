41065f88e1fb942e789d51a5849a4002dcab85af
Ronen Zolicha <ronen1245@gmail.com>
2019-12-09 17:06:24 +0200

    FeedAdapter creation

---
+++ app/src/main/java/zoli/top10downloader/FeedAdapter.java
@@ -0,0 +1,23 @@
+package zoli.top10downloader;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class FeedAdapter extends ArrayAdapter {
+    private static final String TAG = "FeedAdapter";
+    private final int layoutResource;
+    private final LayoutInflater layoutInflater;
+    private List<FeedEntry> applications;
+
+    public FeedAdapter(@NonNull Context context, int resource, List<FeedEntry> applications) {
+        super(context, resource);
+        this.layoutResource = resource;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.applications = applications;
+    }
+}
