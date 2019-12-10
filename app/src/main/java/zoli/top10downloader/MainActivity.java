c91e1fc188015df7c7579573d1bc7f400655619b
Ronen Zolicha <ronen1245@gmail.com>
2019-12-10 11:34:40 +0200

    Adding menu options

--- app/src/main/java/zoli/top10downloader/MainActivity.java
+++ app/src/main/java/zoli/top10downloader/MainActivity.java
@@ -21,6 +21,8 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private ListView listApps;
+    private String feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
+    private int feedLimit = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
